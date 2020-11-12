f648cab2372c1d1e92626dc7f60980a042f0d256
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 08:51:47 -0500

    spelling: compilation
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- ql/src/java/org/apache/hadoop/hive/ql/reexec/IReExecutionPlugin.java
+++ ql/src/java/org/apache/hadoop/hive/ql/reexec/IReExecutionPlugin.java
@@ -50,7 +50,7 @@ public interface IReExecutionPlugin {
   boolean shouldReExecute(int executionNum);
 
   /**
-   * The plugin should prepare for the re-compilaton of the query.
+   * The plugin should prepare for the re-compilation of the query.
    */
   void prepareToReExecute();
 
