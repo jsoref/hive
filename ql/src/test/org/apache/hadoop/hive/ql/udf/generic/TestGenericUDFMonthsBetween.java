8f56b4e9be548b1fa2fbd7aaa5b87913c23ae454
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 21:15:19 -0500

    spelling: viewed
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- ql/src/test/org/apache/hadoop/hive/ql/udf/generic/TestGenericUDFMonthsBetween.java
+++ ql/src/test/org/apache/hadoop/hive/ql/udf/generic/TestGenericUDFMonthsBetween.java
@@ -88,7 +88,7 @@ public class TestGenericUDFMonthsBetween {
     runTestStr("2000-06-01", "2004-07-01", -49.0, udf);
     // test February of non-leap year, 2/28
     runTestStr("2002-02-28", "2002-03-01", -0.12903226, udf);
-    // test February of non-leap year, 2/31 is viewd as 3/3 due to 3 days diff
+    // test February of non-leap year, 2/31 is viewed as 3/3 due to 3 days diff
     // from 2/31 to 2/28
     runTestStr("2002-02-31", "2002-03-01", 0.06451613, udf);
 
@@ -171,7 +171,7 @@ public class TestGenericUDFMonthsBetween {
     runTestTs("2000-06-01 00:00:00", "2004-07-01 00:00:00", -49.0, udf);
     // test February of non-leap year, 2/28
     runTestTs("2002-02-28 00:00:00", "2002-03-01 00:00:00", -0.12903226, udf);
-    // test February of non-leap year, 2/31 is viewd as 3/3 due to 3 days diff
+    // test February of non-leap year, 2/31 is viewed as 3/3 due to 3 days diff
     // from 2/31 to 2/28
 
     // test Feb of leap year, 2/29
@@ -229,7 +229,7 @@ public class TestGenericUDFMonthsBetween {
     runTestDt("2000-06-01", "2004-07-01", -49.0, udf);
     // test February of non-leap year, 2/28
     runTestDt("2002-02-28", "2002-03-01", -0.12903226, udf);
-    // test February of non-leap year, 2/31 is viewd as 3/3 due to 3 days diff
+    // test February of non-leap year, 2/31 is viewed as 3/3 due to 3 days diff
     // from 2/31 to 2/28
     runTestDt("2002-02-31", "2002-03-01", 0.06451613, udf);
 
